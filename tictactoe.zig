71a9ae6984dd48bb09482cf1e043bb437c8f4ac3
ayaan momin <am44002srmist.edu.in>
2025-08-10 14:48:54 +0530

    second commit

--- tictactoe.zig
+++ tictactoe.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+const gamestatus = enum { running, win1, win2, draw };
+
+fn drawBoard() void {}
+
+fn getPlayerInput() !u8 {}
+
+fn makeChoice() !void {}
+
+fn checkWin() gamestatus {}
+
 pub fn main() void {
     std.debug.print("hello {s}", .{"world"});
 }
